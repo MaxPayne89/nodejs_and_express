8c84296356ae4e2d50a16eefd4e68aaf96f7b8db
maximilianpergl <maximilian.pergl@eon-energie.com>
2020-04-04 12:54:05 +0200

    state of development

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { data } = require('../data.json')
-const { projects } = data.projects
+const { data } = require('../data/data.json')
+const { projects } = data
 
 router.get('/', (req, res) => {
-    const templateData = projects
-    res.render('index', templateData)
+    res.render('index', { projects })
 })
 
 router.get('/about', (req, res) => {
